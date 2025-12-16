7d13e606499882400a257169ff4721ad141071ca
Don Young <don.f.young@gmail.com>
2025-12-15 19:22:32 -0500

    Print rectangle use char.

---
+++ ch1_rectangle_printer.py
@@ -0,0 +1,23 @@
+def print_chars():
+    width = 0
+
+    while True:
+        try:
+            width = int(input("Enter width of rectangle (max 25): "))
+
+            if width > 25:
+                print(f"Error: ", width, " is too high. Please enter 25 or lower.")
+            elif width <= 0:
+                print("Error: please enter a positive number.")
+            else:
+                # if valid, break out of the loop
+                break
+
+        except ValueError:
+            print("Invalid input. Please enter a whole number")
+        
+    for i in range(5):
+        print("O" * width)
+
+# Run function1
+print_chars()
